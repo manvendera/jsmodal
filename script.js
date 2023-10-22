681139c7dc645b6998f3858dca7777a60abc8c06
manvendera pathak <manvenderapathak165@gmail.com>
2023-10-22 07:24:22 +0530

    script file some few changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
+//   Modal open function
+const openModal = () => {
+  console.log("Modal is Open");
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+};
+
